ec797b7fd2c3e74967fd5ac2ada24d8e3d7a2617
Camila Rinaldi <camiivelleda@gmail.com>
2025-01-21 11:47:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,3 @@
-#Importação das bibliotecas 
-import requests
-import pandas as pd
-import plotly.express as px
-import folium
-from streamlit_folium import st_folium, folium_static
-import streamlit as st
-import time
-from altair import Chart
-import plotly.figure_factory as ff
-import geopandas as gpd
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
- 
 # Configurações da página
 st.set_page_config(
     page_title="Simulídeos",
@@ -36,9 +22,6 @@ coordenadorias = gpd.read_file('https://raw.githubusercontent.com/camilarinaldi/
 
 # Carrega dados geoespaciais do novo arquivo .geojson
 calhas = gpd.read_file('https://raw.githubusercontent.com/camilarinaldi/CEVS/refs/heads/main/calhas%20fluviais.geojson')
-
-import plotly.graph_objects as go
-import geopandas as gpd
 
 # Inicializar o mapa
 fig = go.Figure()
