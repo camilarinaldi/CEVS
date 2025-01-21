c5b07404118083a5e3a0d1f4dfa78bcc31f54ba0
Camila Rinaldi <camiivelleda@gmail.com>
2025-01-21 11:47:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
+#Importação das bibliotecas 
+import requests
+import pandas as pd
+import plotly.express as px
+import folium
+from streamlit_folium import st_folium, folium_static
+import streamlit as st
+import time
+from altair import Chart
+import plotly.figure_factory as ff
+import geopandas as gpd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplotsimport plotly.graph_objects as go
+import geopandas as gpd
+
 # Configurações da página
 st.set_page_config(
     page_title="Simulídeos",
