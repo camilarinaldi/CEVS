4d398a571cc231fe7ea37ca8e8aeb8673fb9fbd2
Camila Rinaldi <camiivelleda@gmail.com>
2025-02-03 13:57:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,8 +80,6 @@ fig.update_layout(
     mapbox=dict(
         style="carto-positron",
         center={'lat': -30.452349861219243, 'lon': -53.55320517512141},
-        width=1000,
-        height=700,
         zoom=5.3
     ),
     margin={"r": 0, "t": 0, "l": 0, "b": 0},
